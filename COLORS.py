f2e8c5bf096291329077c50f1301ccb2e90cae13
Duncan <121634221+DunkeyShowsCode@users.noreply.github.com>
2023-01-02 08:28:56 -0800

    Add files via upload

---
+++ COLORS.py
@@ -0,0 +1,11 @@
+class COLORS:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\33[96m'
+    GREEN = '\33[92m'
+    YELLOW = '\33[93m'
+    RED = '\33[91m'
+    TERMINATE = '\33[0m'
+    BLOD = '\33[1m'
+    UNDERLINE = '\33[4m'
+    # This is just declaring the colors that can be used.
